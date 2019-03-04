056ba05911cff822b990f28aef46f34130a225ae
Elliott Porter <elliott@weblake.co.uk>
2019-03-04 20:31:44 +0000

    Initial commit

---
+++ up-and-going/phone-app.js
@@ -0,0 +1,82 @@
+const SPENDING_THRESHOLD = 200;
+const TAX_RATE = 0.08;
+const PHONE_PRICE = 99.99;
+const ACCESSORY_PRICE = 9.99;
+
+var bankBalance = prompt( 'What is your bank balance?:' );
+var amount;
+
+function calculateTheTax( amount ) {
+    return amount * TAX_RATE;
+}
+
+function formatThePrice( amount ) {
+    return '£' + amount.toFixed( 2 );
+}
+
+// keep buying phones while you still have the money
+while( amount < bankBalance ) {
+    amount = amount + PHONE_PRICE;
+
+    if( amount < SPENDING_THRESHOLD ) {
+        amount = amount + ACCESSORY_PRICE;
+    }
+}
+
+// pay the tax
+amount = amount + calculateTheTax( amount );
+
+console.log(
+    'Your purchase is:' + formatThePrice( amount )
+);
+
+// can you afford the purchase?
+if( amount > bankBalance ) {
+    console.log(
+        'You can\'t afford this phone. Sorry'
+    );
+}
+
+// test
+
+const SPENDING_THRESHOLD = 200;
+const TAX_RATE = 0.08;
+const PHONE_PRICE = 99.99;
+const ACCESSORY_PRICE = 9.99;
+
+var bank_balance = 303.91;
+var amount = 0;
+
+function calculateTax(amount) {
+    return amount * TAX_RATE;
+}
+
+function formatAmount(amount) {
+    return "$" + amount.toFixed( 2 );
+}
+
+while (amount < bank_balance) {
+    // buy a new phone!
+    amount = amount + PHONE_PRICE;
+
+    // can we afford the accessory?
+    if (amount < SPENDING_THRESHOLD) {
+        amount = amount + ACCESSORY_PRICE;
+    }
+}
+
+// don't forget to pay the government, too
+amount = amount + calculateTax( amount );
+
+console.log(
+    "Your purchase: " + formatAmount( amount )
+);
+// Your purchase: $334.76
+
+// can you actually afford this purchase?
+if (amount > bank_balance) {
+    console.log(
+        "You can't afford this purchase. :("
+    );
+}
+// You can't afford this purchase. :(
